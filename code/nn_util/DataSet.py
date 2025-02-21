c8350e7f626d6d885ffdc6dbd99c40f9bdb00205
klappstuhl24 <t.kleipsties@web.de>
2025-02-21 14:07:05 +0100

    updated dataloader

--- code/nn_util/DataSet.py
+++ code/nn_util/DataSet.py
@@ -35,45 +35,55 @@ class EyeDiameterDataset(Dataset):
 class EyeBinaryMaskDataset(Dataset):
     def __init__(self, annotations_file: str):
         self.img_labels = pd.read_csv(annotations_file)
+        self.data = []
+        self.labels = []
+        self.__preprocess_data()
+        self.__preprocess_labels()
+        
+    def __preprocess_data(self):
+        for idx in range(len(self.img_labels)):
+            img_path = self.img_labels.iloc[idx, 0]
+            image = read_image(img_path).float()
+            
+            if image.shape[0] == 4:
+                image = image[:3, :, :]
+            
+            img_transform = transforms.Compose([
+                transforms.Grayscale(num_output_channels=1),
+                transforms.CenterCrop(min(image.shape[1:])),
+                transforms.ConvertImageDtype(torch.float),
+                transforms.Normalize(mean=[0.5], std=[0.5])
+            ])
+            
+            image = img_transform(image)
+            self.data.append(image)
+            
+    def __preprocess_labels(self):
+        for idx in range(len(self.img_labels)):
+            label_path = self.img_labels.iloc[idx, 1]
+            label = read_image(label_path).float()
+            
+            if label.shape[0] == 4:
+                label = label[:3, :, :]
+            
+            mask_transform = transforms.Compose([
+                transforms.Grayscale(num_output_channels=1),
+                transforms.CenterCrop(min(label.shape[1:])),
+                transforms.ConvertImageDtype(torch.float)
+            ])
+            
+            label = mask_transform(label)
+            label = (label > 200).float()
+            self.labels.append(label)
        
     def __len__(self):
         return len(self.img_labels)
    
     def __getitem__(self, idx):
-        img_path = self.img_labels.iloc[idx, 0]
-        image = read_image(img_path).float()
-       
-        if image.shape[0] == 4:
-            image = image[:3, :, :]
-       
-        img_transform = transforms.Compose([
-            transforms.Grayscale(num_output_channels=1),
-            transforms.CenterCrop(min(image.shape[1:])),
-            transforms.ConvertImageDtype(torch.float),
-            transforms.Normalize(mean=[0.5], std=[0.5])
-        ])
-       
-        image = img_transform(image)
-       
-        label_path = self.img_labels.iloc[idx, 1]
-        label = read_image(label_path).float()
-       
-        if label.shape[0] == 4:
-            label = label[:3, :, :]
-           
-        mask_transform = transforms.Compose([
-            transforms.Grayscale(num_output_channels=1),
-            transforms.CenterCrop(min(label.shape[1:])),
-            transforms.ConvertImageDtype(torch.float)
-        ])
-        
-        label = mask_transform(label)
-        label = (label > 200).float()
-        
-        return image, label
+        return self.data[idx], self.labels[idx]
 
 
-def create_data_loader(dataset_path: str, dataset_type: object = EyeDiameterDataset, batch_size: int = 16) -> torch.utils.data.DataLoader:
+def create_data_loader(dataset_path: str, dataset_type: object = EyeDiameterDataset, batch_size: int = 64) -> torch.utils.data.DataLoader:
     dataset = dataset_type(dataset_path)
     dataloader = torch.utils.data.DataLoader(
         dataset=dataset,
